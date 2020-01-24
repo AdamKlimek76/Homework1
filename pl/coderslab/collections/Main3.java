f7c7f59fbf5019e3bfe0c6f93938795024975459
Adam Klimek <a.klim@op.pl>
2020-01-24 19:45:29 +0100

    Homework1 by Adam Klimek

---
+++ pl/coderslab/collections/Main3.java
@@ -0,0 +1,28 @@
+package pl.coderslab.collections;
+
+import java.util.regex.Pattern;
+
+public class Main3 {
+
+    public static void main(String[] args) {
+        String email="_bcBB123de1-";
+        System.out.println(verifyLogin(email));
+    }
+
+    static boolean verifyLogin(String email) {
+        return verifyNumberLoginCases(email) && verifyLoginCases(email) && verifyLoginFirstCase(email);
+    }
+
+    static boolean verifyNumberLoginCases(String email) {
+        return Pattern.matches(".{5,}", email);
+    }
+
+    static boolean verifyLoginCases(String email) {
+        return Pattern.matches("(\\w*\\-*)+", email);
+    }
+
+    static boolean verifyLoginFirstCase(String email) {
+        return Pattern.matches("\\D.+", email);
+    }
+
+}
